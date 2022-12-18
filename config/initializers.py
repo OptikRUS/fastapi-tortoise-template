aec77d514cd921d456a2f30a61de5f3286a36cdb
optikrus <optikrus61@gmail.com>
2022-12-18 18:54:03 +0300

    add init exceptions

--- config/initializers.py
+++ config/initializers.py
@@ -3,14 +3,15 @@ from fastapi import FastAPI
 
 def init_app() -> FastAPI:
     from config import app_config
-    app = FastAPI(**app_config)
 
+    app = FastAPI(**app_config)
     return app
 
 
 def init_cors(application: FastAPI) -> None:
     from fastapi.middleware.cors import CORSMiddleware
     from config import cors_config
+
     application.add_middleware(CORSMiddleware, **cors_config)
 
 
@@ -26,3 +27,11 @@ def init_database(application: FastAPI) -> None:
     from config import tortoise_config
 
     register_tortoise(application, **tortoise_config)
+
+
+def init_exception_handlers(application: FastAPI) -> None:
+    from config.exceptions import get_exceptions
+    from handlers import common_exception_handler
+
+    for exception in get_exceptions():
+        application.add_exception_handler(exception, common_exception_handler)
