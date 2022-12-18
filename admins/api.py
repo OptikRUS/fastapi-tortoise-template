c898f256995037a738983a3cd2578427d529f43c
optikrus <optikrus61@gmail.com>
2022-12-19 00:38:20 +0300

    edit admin case description

--- admins/api.py
+++ admins/api.py
@@ -12,7 +12,7 @@ admins_router = APIRouter(prefix="/admins", tags=["admins"])
 @admins_router.get("/users", response_model=list[UserForAdminResponse] | UserForAdminResponse)
 async def get_users_for_admin(user_id: int = Query(None), current_admin: User = Depends(get_current_admin)):
     """
-    Список пользователей для админа
+    Получение пользователей для админа
     """
 
     users = GetUsersForAdmin()
