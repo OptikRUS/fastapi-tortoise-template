73ef32ef51ebe8711f60ebecbdf7d0624b2ddc8b
optikrus <optikrus61@gmail.com>
2022-12-18 23:24:25 +0300

    edit library cases

--- library/use_cases.py
+++ library/use_cases.py
@@ -1,9 +1,6 @@
 from .schemas import CreateGenre, GenreResponse, CreateAuthor, AuthorResponse, CreateBook, BookResponse
 from .models import Genre, Author, Book
-from .exceptions import (
-    GenreAlreadyExistError, GenresNotFoundError, AuthorAlreadyExistError, AuthorsNotFoundError,
-    BookAlreadyExistError, BooksNotFoundError, BookNotFoundError
-)
+from library import exceptions as exc
 
 
 class GenreCreationCase:
@@ -15,7 +12,7 @@ class GenreCreationCase:
         genre_exist = await Genre.exists(name__iexact=genre.name)
 
         if genre_exist:
-            raise GenreAlreadyExistError
+            raise exc.GenreAlreadyExistError
         new_genre = await Genre.create(**genre.dict())
         return GenreResponse.from_orm(new_genre)
 
@@ -29,7 +26,7 @@ class GetGenresCase:
         all_genres = await Genre.all()
         if all_genres:
             return all_genres
-        raise GenresNotFoundError
+        raise exc.GenresNotFoundError
 
 
 class AuthorCreationCase:
@@ -45,7 +42,7 @@ class AuthorCreationCase:
         )
 
         if author_exist:
-            raise AuthorAlreadyExistError
+            raise exc.AuthorAlreadyExistError
         new_author = await Author.create(**author.dict())
         return AuthorResponse.from_orm(new_author)
 
@@ -59,7 +56,7 @@ class GetAuthorsCase:
         all_authors = await Author.all()
         if all_authors:
             return all_authors
-        raise AuthorsNotFoundError
+        raise exc.AuthorsNotFoundError
 
 
 class BookCreationCase:
@@ -71,27 +68,29 @@ class BookCreationCase:
 
         genres = await Genre.filter(id__in=book.genres_id)
         if not genres:
-            raise GenresNotFoundError
+            raise exc.GenresNotFoundError
 
-        authors = await Author.filter(id__in=book.authors_id)
-        if not authors:
-            raise AuthorsNotFoundError
+        author = await Author.filter(id=book.author_id)
+        if not author:
+            raise exc.AuthorNotFoundError
 
         book_exist = await Book.exists(title__iexact=book.title)
         if book_exist:
-            raise BookAlreadyExistError
+            raise exc.BookAlreadyExistError
 
+        print()
         new_book = await Book.create(**book.dict())
-        await new_book.authors.add(*authors)
+        print()
+        # await new_book.author.add(author)
         await new_book.genres.add(*genres)
         await new_book.save()
 
-        created_book = await Book.get(id=new_book.id)
+        created_book = await Book.get(id=new_book.id).select_related('author')
 
         # отдельные запросы в бд для связных полей
         book_response = dict(
             created_book,
-            authors=await created_book.authors,
+            author=created_book.author,
             genres=await created_book.genres
         )
         return BookResponse(**book_response)
@@ -105,19 +104,19 @@ class GetBooksCase:
     async def __call__(self):
         books_response = list()
 
-        all_books = await Book.all().prefetch_related('authors', 'genres')
+        all_books = await Book.all().prefetch_related('author', 'genres')
         if all_books:
             for book in all_books:
                 print()
                 books_response.append(
                     dict(
                         book,
-                        authors=await book.authors,
+                        author=book.author,
                         genres=await book.genres
                     )
                 )
             return books_response
-        raise BooksNotFoundError
+        raise exc.BooksNotFoundError
 
 
 class GetBookCase:
@@ -131,8 +130,8 @@ class GetBookCase:
         if book:
             book_response = dict(
                 book,
-                authors=await book.authors,
+                author=await book.author,
                 genres=await book.genres
             )
             return BookResponse(**book_response)
-        raise BookNotFoundError
+        raise exc.BookNotFoundError
