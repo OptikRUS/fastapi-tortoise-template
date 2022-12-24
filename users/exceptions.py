089bfbc2400f437847370ee69bc53646498498ff
optikrus <optikrus61@gmail.com>
2022-12-24 21:08:27 +0300

    edit user exc

--- users/exceptions.py
+++ users/exceptions.py
@@ -1,5 +1,5 @@
 from fastapi import status
-from .entities import BaseUserException
+from .entities import BaseUserException, BaseUserHTTPException
 
 """
 Пример через Base Exception
@@ -39,20 +39,19 @@ class UserNotAuthError(UserWrongPasswordError):
     reason: str = "user_not_auth"
 
 
-class UserNotActiveErrorHTTP(BaseUserException):
-    message: str = "Пользователь неактивен."
-    status: int = status.HTTP_403_FORBIDDEN
-    reason: str = "user_not_active"
-
-
-class UserForbiddenError(BaseUserException):
-    message: str = "У пользователя нет прав."
-    status: int = status.HTTP_403_FORBIDDEN
-    reason: str = "user_have_not_permissions"
-    headers: dict = {"WWW-Authenticate": "Bearer"}
-
-
 class UserNotFoundError(BaseUserException):
     message: str = "Пользователь не найден."
     status: int = status.HTTP_404_NOT_FOUND
     reason: str = "user_not_found"
+
+
+class UserNotActiveErrorHTTP(BaseUserHTTPException):
+    message: str = "Пользователь неактивен."
+    status: int = status.HTTP_403_FORBIDDEN
+    reason: str = 'user_not_active'
+    headers: dict = {"WWW-Authenticate": "Bearer"}
+
+
+class UserForbiddenErrorHTTP(UserNotActiveErrorHTTP):
+    message: str = "У пользователя нет прав."
+    reason: str = 'user_forbidden_error'
