871199c538f7213f4cb93fbc9a3bdeb861c353c9
optikrus <optikrus61@gmail.com>
2023-01-07 19:29:27 +0300

    edit base error model

--- common/exceptions/models.py
+++ common/exceptions/models.py
@@ -2,6 +2,10 @@ from pydantic import BaseModel, Field
 
 
 class BaseExceptionModel(BaseModel):
+    """
+    Базовая модель ошибки
+    """
+
     message: str = Field("Сообщение об ошибке.")
     reason: str = Field("error_reason")
     ok: bool = Field(False)
