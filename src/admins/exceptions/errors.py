1f5caa744598ce548394449fa2e5a2c59868e5fb
optikrus <optikrus61@gmail.com>
2023-01-07 19:24:01 +0300

    edit admin error

--- src/admins/exceptions/errors.py
+++ src/admins/exceptions/errors.py
@@ -6,5 +6,8 @@ from common.exceptions import BaseBadRequestError
 
 
 class AdminSelfMatchError(BaseBadRequestError):
+    """
+    Ошибка редактирования самого себя
+    """
     message: str = "Недоступно для себя."
     reason: str = "self_match_error"
