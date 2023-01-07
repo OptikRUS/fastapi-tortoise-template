5d1b2b88b480c952f16cf84e3fe918e44063874a
optikrus <optikrus61@gmail.com>
2023-01-07 19:19:15 +0300

    edit admin approve & block model

--- src/admins/models/user.py
+++ src/admins/models/user.py
@@ -2,8 +2,14 @@ from .base import UserUpdateResponse
 
 
 class UserApprovedResponse(UserUpdateResponse):
+    """
+    Модель подтверждения пользователя
+    """
     is_approved: bool
 
 
 class UserBlockedResponse(UserUpdateResponse):
+    """
+    Модель блокировки/разблокировки пользователя
+    """
     is_active: bool
