6b89e53c02fd02b45117ad4c53376973661d32b3
optikrus <optikrus61@gmail.com>
2023-01-07 19:17:55 +0300

    edit admin response model

--- src/admins/models/base.py
+++ src/admins/models/base.py
@@ -4,6 +4,10 @@ from pydantic import BaseModel, Field
 
 
 class UserUpdateResponse(BaseModel):
+    """
+    Базовая модель для обновления пользователя
+    """
+
     id: int
     username: str = Field("username")
     updated_at: datetime
